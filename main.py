c40254fb110d0a8230a2cfa70cc351818258ecd5
Jamie McNally <17f23206@marling.school>
2020-09-23 08:22:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+options=["red", "green","blue"]
+selection=random.choice(options)
+print(selection)
+print(selection)
+print(selection)
+print(selection)
+print(selection)
+print(selection)
+print(selection)
