ece8f255798d569d40be17903254dc0ce8b92686
Jamie McNally <17f23206@marling.school>
2020-09-23 08:23:07 +0000

    import random

--- main.py
+++ main.py
@@ -2,9 +2,3 @@ import random
 options=["red", "green","blue"]
 selection=random.choice(options)
 print(selection)
-print(selection)
-print(selection)
-print(selection)
-print(selection)
-print(selection)
-print(selection)
